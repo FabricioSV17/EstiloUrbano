052125db5996593b0fefa44b58433cc86941cb89
MiaRous <miarosa4to@gmail.com>
2023-12-04 06:50:43 -0500

    Incorporando Carpeta - EstiloUrbanoEmpresa - Conexion Productos

---
+++ EstiloUrbanoEmpresa/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url ="http://localhost/ProyectoSistemaG/EstiloUrbanoEmpresa/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "sistematienda";
+const charset = "charset=utf8";
+?>
